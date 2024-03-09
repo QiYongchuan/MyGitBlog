03b3219a715e543029582469368c06eb576e710c
QI <105039020+QiYongchuan@users.noreply.github.com>
2024-03-09 21:52:16 +0800

    Update main.py

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ My personal blog using issues and GitHub Actions (参考[yihong](https://github.
 * 用文字记录我的胡思乱想与生活的瞬间，我疯狂的想法与可能为之的行动。  
 * 一个从[农学研究生退学](https://github.com/QiYongchuan/MyGitBlog/issues/84)转行到计算机行业的年轻人。
 * 记录在这个时代下的焦虑、迷茫、挣扎与希望。
-* 主页[About me](https://github.com/QiYongchuan/Qiyongchuan)
+* [About me](https://github.com/QiYongchuan/Qiyongchuan)
 
 ### 提醒自己：
 #### 1.放弃向他人证明自己，放弃向自己证明自己。专注忘我地去做你应该做的事情，心无旁骛地去解决问题。当你脚踏实地的走自己的路时，那种拼命想要证明什么的冲动就会越来越少。你也会因此变得轻松、自由。
