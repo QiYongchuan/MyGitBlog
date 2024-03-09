05927252f6643692bda0d5622e4ebad9237d59ab
QI <105039020+QiYongchuan@users.noreply.github.com>
2024-03-09 21:48:23 +0800

    Update main.py

--- main.py
+++ main.py
@@ -10,6 +10,7 @@ from marko.ext.gfm import gfm as marko
 
 MD_HEAD = """## MyGitBlog
 My personal blog using issues and GitHub Actions (参考[yihong](https://github.com/yihong0618/gitblog))
+[About me](https://github.com/QiYongchuan/Qiyongchuan)
 
 * 用文字记录我的胡思乱想与生活的瞬间，我疯狂的想法与可能为之的行动。  
 * 一个从[农学研究生退学](https://github.com/QiYongchuan/MyGitBlog/issues/84)转行到计算机行业的年轻人。
